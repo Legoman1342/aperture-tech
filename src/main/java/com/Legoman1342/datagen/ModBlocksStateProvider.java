c365a556efa5aa9fb15a4314b3f2f53ac98735be
Legoman1342 <lex.w.johnson@gmail.com>
2022-06-12 18:09:01 -0400

    Created multipart versions of catwalk models

--- src/main/java/com/Legoman1342/datagen/ModBlocksStateProvider.java
+++ src/main/java/com/Legoman1342/datagen/ModBlocksStateProvider.java
@@ -1,4 +1,22 @@
 package com.Legoman1342.datagen;
 
-public class ModBlocksStateProvider {
+import com.Legoman1342.aperturetech.ApertureTech;
+import com.Legoman1342.blocks.custom.Catwalk;
+import com.Legoman1342.setup.Registration;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import static com.Legoman1342.blocks.custom.Catwalk.CATWALK_END;
+
+public class ModBlocksStateProvider extends BlockStateProvider {
+    public ModBlocksStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
+        super(gen, ApertureTech.MODID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        this.getMultipartBuilder(Registration.catwalk.get())
+                //TODO Implement catwalk multipart models (https://mcforge.readthedocs.io/en/1.18.x/datagen/client/modelproviders/)
+    }
 }
